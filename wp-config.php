72107614cc8666445edb7718ee6efe1f7177f5f1
Andre Lima Mendes <andreachille24@gmail.com>
2016-03-07 15:46:26 -0300

    My First Contatc with GitHub

--- wp-config.php
+++ wp-config.php
@@ -84,6 +84,6 @@ define('WP_DEBUG', false);
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
-
-/** Sets up WordPress vars and included files. */
+"/**My First contatc"*/
+"/** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
